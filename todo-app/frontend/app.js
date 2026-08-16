11b2b13c3b9089054813fe8fa6350198b08891d5
ahmetpylnn <ahmet20alya20@gmail.com>
2026-08-16 20:06:03 +0300

    frontend 1

--- todo-app/frontend/app.js
+++ todo-app/frontend/app.js
@@ -1,15 +1,27 @@
-const form= document.getElementById('todoForm');
-const input= document.getElementById('todoInput');
-const list= document.getElementById('taskList');
+// HTML içindeki gerekli elementleri ID'lerine göre seçiyoruz
+const form = document.getElementById('todoForm');
+const input = document.getElementById('todoInput');
+const taskList = document.getElementById('taskList');
 
+// Form gönderildiğinde (Ekle butonuna basıldığında) çalışacak olay
 form.addEventListener('submit', function(e) {
+    // Sayfanın yenilenmesini engelliyoruz (çok önemli!)
     e.preventDefault();
-    const text= input.value.trim();
-    if (text !== '') return;
+
+    // Input içine yazılan metni alıyoruz ve sağ/sol boşluklarını siliyoruz
+    const text = input.value.trim();
+    
+    // Eğer input boşsa hiçbir şey yapma
+    if (text === '') return;
+
+    // Hafızada yeni bir <li> etiketi üretiyoruz
     const li = document.createElement('li');
     li.textContent = text;
 
+    // Ürettiğimiz bu <li> etiketini ekrandaki <ul> listesinin içine ekliyoruz
     taskList.appendChild(li);
+
+    // İşlem bittikten sonra inputu temizliyoruz ve odaklıyoruz
     input.value = '';
     input.focus();
 });
