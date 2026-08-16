feeff3e907956fa4a2ee2e7f73935891af9a9182
ahmetpylnn <ahmet20alya20@gmail.com>
2026-08-16 20:10:44 +0300

    Frontend kodlari yeni branch'e eklendi

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +130,10 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+
+
+CORS_ALLOW_ALL_ORIGINS = True
